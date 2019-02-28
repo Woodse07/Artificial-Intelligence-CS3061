d65cec32198aa5353c7e6ab2f3181c1e50d9b011
Woodse07 <sewoods@tcd.ie>
2019-02-28 17:52:36 +0000

    Some progress on A* algorithm

--- Assignment_1/incomplete.pl
+++ Assignment_1/incomplete.pl
@@ -22,6 +22,26 @@ mkList(X,[X]).
 
 initKB(File) :- retractall(kb(_)), makeKB(File).
 
-astar(Node,Path,Cost) :- kb(KB), astar(Node,Path,Cost,KB).
+%----------------------------------------------------------------
 
-% astar(Node,Path,Cost,KB) :- ???
+astar(Node,Path,Cost) :- kb(KB), 
+						 astar(Node,Path,Cost,KB).
+
+astar([[Node, Path, Cost]|_], Path, Cost, _) :- goal(Node).
+astar([[Node, Path, Cost]|Rest],Path,Cost,KB) :- findall(X, arc(Node, X), Children),
+							add-to-frontier(Children, More, New),
+							astar(New, Path, Cost, KB).
+
+less-than([[Node1|_],Cost1],[[Node2|_],Cost2]) :- heuristic(Node1,Hvalue1), 					                              heuristic(Node2,Hvalue2),
+                                                  F1 is Cost1+Hvalue1, 
+                                                  F2 is Cost2+Hvalue2,
+                                                  F1 =< F2.
+
+arc([H|T],Node,Cost,KB) :- member([H|B],KB), 
+						   append(B,T,Node),
+						   length(B,L), 
+						   Cost is L+1.
+
+heuristic(Node, H) :- length(Node, H).
+
+goal([]).
